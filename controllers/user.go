ca22cad977cf1bbee39f8348a23701b3afdd5891
Fishmansky <paul96guitar@gmail.com>
2023-10-27 20:34:20 +0200

    JWT tokens introduced

--- controllers/user.go
+++ controllers/user.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Fishmansky/noteflow/inits"
+	"github.com/Fishmansky/noteflow/middleware"
 	"github.com/Fishmansky/noteflow/models"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -40,6 +41,34 @@ func Login(c *gin.Context) {
 	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password)) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Wrong password",
+		})
+		return
+	}
+	accessToken, err := middleware.CreateAccessToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error": "Failed to create access token",
+		})
+		return
+	}
+	refreshToken, err := middleware.CreateRefreshToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error": "Failed to create refresh token",
+		})
+		return
+	}
+	saveAccessErr := middleware.SaveToken(user.ID, accessToken)
+	if saveAccessErr != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error": "Failed to save access token",
+		})
+		return
+	}
+	saveRefreshErr := middleware.SaveToken(user.ID, refreshToken)
+	if saveRefreshErr != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error": "Failed to save refresh token",
 		})
 		return
 	}
--- middleware/auth.go
+++ middleware/auth.go
@@ -1,16 +1,70 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"strconv"
+	"time"
 
-func Auth(c *gin.Context) {
+	"github.com/Fishmansky/noteflow/inits"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/twinj/uuid"
+)
 
+type TokenDetails struct {
+	Token string
+	Uuid  string
+	Exp   int64
 }
 
-func CreateToken(c *gin.Context) {
-
+func Authenticate(c *gin.Context) bool {
+	return false
 }
 
-func RefreshToken(c *gin.Context) {
+func CreateAccessToken(userID uint) (*TokenDetails, error) {
+	accessToken := &TokenDetails{}
+	accessToken.Uuid = uuid.NewV4().String()
+	accessToken.Exp = time.Now().Add(time.Minute * 15).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"authorized":  "true",
+		"access_uuid": accessToken.Uuid,
+		"user_id":     userID,
+		"exp":         accessToken.Exp,
+	})
+	var err error
+	accessToken.Token, err = token.SignedString(os.Getenv("SECRET"))
+	return accessToken, err
+}
+
+func CreateRefreshToken(userID uint) (*TokenDetails, error) {
+	refreshToken := &TokenDetails{}
+	refreshToken.Uuid = uuid.NewV4().String()
+	refreshToken.Exp = time.Now().Add(time.Hour * 24 * 90).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"authorized":   "true",
+		"refresh_uuid": refreshToken.Uuid,
+		"user_id":      userID,
+		"exp":          refreshToken.Exp,
+	})
+	var err error
+	refreshToken.Token, err = token.SignedString(os.Getenv("SECRET"))
+	return refreshToken, err
+}
+
+func SaveToken(userID uint, t *TokenDetails) error {
+	Expires := time.Unix(t.Exp, 0)
+	now := time.Now()
+
+	err := inits.RedisDB.Set(t.Uuid, strconv.Itoa(int(userID)), Expires.Sub(now)).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func RefreshTokens(c *gin.Context) {
 
 }
 
