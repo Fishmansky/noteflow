3d3b949c228ff1049d98b376065d5480b1ab1477
Fishmansky <paul96guitar@gmail.com>
2023-10-29 12:57:44 +0100

    Redis connection corrected

--- inits/redisconnect.go
+++ inits/redisconnect.go
@@ -6,16 +6,16 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var RedisDB *redis.Client
+var Redis *redis.Client
 
-func init() {
+func ConnecRedis() {
 	var err error
-	RedisDB := redis.NewClient(&redis.Options{
+	Redis = redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_ADDR"),
 		Password: os.Getenv("REDIS_DB"),
 		DB:       0,
 	})
-	_, err = RedisDB.Ping().Result()
+	_, err = Redis.Ping().Result()
 	if err != nil {
 		panic(err)
 	}
